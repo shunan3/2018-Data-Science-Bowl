db56c62602fcd6d7c21ab158b9d3dbc1b2f0006f
Chenjie Xu <cxuscience@gmail.com>
2018-02-17 00:53:04 +0000

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -35,10 +35,10 @@ parser.add_argument('--resume', default='False', type=bool, metavar='N',
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
 
-best_prec1 = 0
+best_prec = 0
 
 def main():
-    global args, best_prec1
+    global args, best_prec
     args = parser.parse_args()
 
     # create model
@@ -56,7 +56,7 @@ def main():
     if args.resume = True:
         checkpoint = torch.load(checkpoint)
 		start_epoch = checkpoint['epoch']
-        best_prec1 = checkpoint['best_prec1']
+        best_prec = checkpoint['best_prec']
         model.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         print("=> loaded checkpoint (epoch {})"
@@ -75,20 +75,20 @@ def main():
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
-	s_trans = transforms.Compose([
+    s_trans = transforms.Compose([
     transforms.Resize((256,256)),
     transforms.ToTensor(),
     normalize,
     ])
     
-	t_trans = transforms.Compose([
+    t_trans = transforms.Compose([
     transforms.Resize((256,256)),
     transforms.ToTensor(),
     ])
 
-	train_dataset =  image_processing(train_data,s_trans,t_trans)
+    train_dataset =  image_processing(train_data,s_trans,t_trans)
 	
-	# Split dataset in train and validation sets
+    # Split dataset in train and validation sets
  	
 	
     train_sampler = None
@@ -97,8 +97,8 @@ def main():
         train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
-    val_loader = torch.utils.data.DataLoader(        ,
-        batch_size=args.batch_size, shuffle=False,
+    val_loader = torch.utils.data.DataLoader(
+	val_dataset, batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
     if args.evaluate:
@@ -112,16 +112,16 @@ def main():
         train(train_loader, model, criterion, optimizer, epoch)
 
         # evaluate on validation set
-        prec1 = validate(val_loader, model, criterion)
+        prec = validate(val_loader, model, criterion)
 
-        # remember best prec@1 and save checkpoint
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
+        # remember best prec and save checkpoint
+        is_best = prec > best_prec
+        best_prec = max(prec, best_prec)
         save_checkpoint({
             'epoch': epoch + 1,
             'arch': args.arch,
             'state_dict': model.state_dict(),
-            'best_prec1': best_prec1,
+            'best_prec': best_prec,
             'optimizer' : optimizer.state_dict(),
         }, is_best)
 
@@ -130,8 +130,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
+    top = AverageMeter()
 
     # switch to train mode
     model.train()
@@ -150,10 +149,9 @@ def train(train_loader, model, criterion, optimizer, epoch):
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        prec = accuracy(output.data, target)
         losses.update(loss.data[0], input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+        top.update(prec[0], input.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -169,17 +167,15 @@ def train(train_loader, model, criterion, optimizer, epoch):
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                  'Prec {top.val:.3f} ({top.avg:.3f})\t'.format(
                    epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+                   data_time=data_time, loss=losses, top=top))
 
 
 def validate(val_loader, model, criterion):
     batch_time = AverageMeter()
     losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
+    top = AverageMeter()
 
     # switch to evaluate mode
     model.eval()
@@ -195,10 +191,9 @@ def validate(val_loader, model, criterion):
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        prec = accuracy(output.data, target)
         losses.update(loss.data[0], input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+        top.update(prec[0], input.size(0))
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -208,15 +203,13 @@ def validate(val_loader, model, criterion):
             print('Test: [{0}/{1}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                  'Prec {top.val:.3f} ({top.avg:.3f})\t''.format(
                    i, len(val_loader), batch_time=batch_time, loss=losses,
-                   top1=top1, top5=top5))
+                   top=top))
 
-    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
-          .format(top1=top1, top5=top5))
+    print(' * Prec {top.avg:.3f}'.format(top=top))
 
-    return top1.avg
+    return top.avg
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
@@ -250,20 +243,8 @@ def adjust_learning_rate(optimizer, epoch):
         param_group['lr'] = lr
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
+def accuracy(output, target):
 
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
 
 def load_data(file_path, has_mask=True):
     file_path = Path(file_path)
